7d38eda0d14fb6c5757b799ad4538279bcae4fd6
Kate Mills <kate.mills1124@gmail.com>
2021-03-20 08:53:01 -0700

    branding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <h4>Skincare Webstore Best Sellers</h4>
 }
 
 export default App
